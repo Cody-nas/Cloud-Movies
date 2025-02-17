a1008b8adae48e4d8f44de0d9cf247a820d44769
Cody-nas <nasiruadamu214@gmail.com>
2025-02-17 13:12:10 +0100

    Update favicon to movie logo and clean up App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
+
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <h1 className="text-3xl font-semibold text-red-500 ">
