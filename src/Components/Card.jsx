b8946821b3cea753f9b7580de2c60e287ab7c458
Cody-nas <nasiruadamu214@gmail.com>
2025-02-18 15:13:12 +0100

    Update Card component layout to improve genre display styling

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,7 +5,7 @@ const Card = () => {
     <div className="h-[650px] relative flex justify-center items-center shrink-0 p-2 bg-gray-800 group">
       <div className="w-[97%] h-[97%] mx-auto text-white absolute rounded-lg bg-black/50 flex flex-col justify-center gap-y-2 p-10 cursor-pointer ">
         <h1 className="text-4xl">The Godfather</h1>
-        <div className="">
+        <div className="flex gap-x-2 items-center ">
           <span>Genres:</span>
           <span>Crime Drama</span>
         </div>
