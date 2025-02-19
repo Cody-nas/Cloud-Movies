9180ef50b4cd8ebd5709c7588ec2b2f03366810c
Cody-nas <nasiruadamu214@gmail.com>
2025-02-19 08:06:16 +0100

    Refactor Card component: update image source, enhance styling for release date and summary

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -13,14 +13,18 @@ const Card = () => {
           Original Language: <span className="mr-2 uppercase"> EN</span>
         </span>
         <span className="flex gap-x-2 ">
-          Release Date: <span>1972-03-04</span>
+          Release Date: <span className="mr-2 text-yellow-400">1972-03-04</span>
         </span>
-        <p>
-          <span>Summary:</span>
-          <span> Summary text</span>
+        <p className="flex flex-col gap-y-1">
+          <span className="text-red-500">Summary:</span>
+          <span className="first-letter:pl-2"> Summary text</span>
         </p>
       </div>
-      <img src="./god.jpeg" alt="movie image" />
+      <img
+        src="./god.png"
+        alt="movie image"
+        className="object-cover rounded-xl opacity-50 group-hover:opacity-100 transition-opacity duration-500"
+      />
     </div>
   );
 };
