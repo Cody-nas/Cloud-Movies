5104339171f1b21085a1fa430df6e42b9920e99e
Cody-nas <nasiruadamu214@gmail.com>
2025-02-19 06:19:19 +0100

    Refine Card component styling for genre and language display

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,13 +6,13 @@ const Card = () => {
       <div className="w-[97%] h-[97%] mx-auto text-white absolute rounded-lg bg-black/50 flex flex-col justify-center gap-y-2 p-10 cursor-pointer ">
         <h1 className="text-4xl">The Godfather</h1>
         <div className="flex gap-x-2 items-center ">
-          <span>Genres:</span>
-          <span>Crime Drama</span>
+          <span className="text-lg">Genres:</span>
+          <span className="font-semibold text-red-800">Crime Drama</span>
         </div>
-        <span>
-          Original Language: <span> EN</span>
+        <span className="flex gap-x-2">
+          Original Language: <span className="mr-2 uppercase"> EN</span>
         </span>
-        <span>
+        <span className="flex gap-x-2 ">
           Release Date: <span>1972-03-04</span>
         </span>
         <p>
