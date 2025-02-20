5c0e6c6174ed9efee4c8fb7eaaa71b5ec8171b39
Cody-nas <nasiruadamu214@gmail.com>
2025-02-20 16:43:51 +0100

    Add toggle functionality to show/hide movie description in Card component

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import { useState } from "react";
 
 const Card = ({ cardWidth, movie }) => {
   const { title, image, genres, originalLanguage, releaseDate, overview } =
     movie;
+  const [showDesc, setShowDesc] = useState(false);
   return (
     <div
       style={{ width: cardWidth }}
       className="h-[650px] relative flex justify-center items-center shrink-0 p-2 bg-gray-800 group"
     >
-      <div className="w-[97%] h-[97%] mx-auto text-white absolute rounded-lg bg-black/50 flex flex-col justify-center gap-y-2 p-10 cursor-pointer ">
+      <div
+        onClick={() => setShowDesc(!showDesc)}
+        className="w-[97%] h-[97%] mx-auto text-white absolute rounded-lg bg-black/50 flex flex-col justify-center gap-y-2 p-10 cursor-pointer "
+      >
         <h1 className="text-4xl">{title}</h1>
         <div className="flex gap-x-2 items-center ">
           <span className="text-lg">Genres:</span>
