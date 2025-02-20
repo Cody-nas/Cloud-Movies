78e14ff9d6114848ae11cc445d6b66987c865952
Cody-nas <nasiruadamu214@gmail.com>
2025-02-20 10:18:58 +0100

    Fetch movie data in Home component; implement useEffect for API call

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,37 @@
 import Card from "./Card";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Home = () => {
   const [cardWidth, setCardWidth] = useState(500);
   const cardInRow = 5;
   const [wrapperWidth, setWrapperWidth] = useState(cardWidth * cardInRow);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const url =
+        "https://tvshow.p.rapidapi.com/Movie/NowPlaying?Page=1&Language=en-US&Adult=true";
+      const options = {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key":
+            "b07ef8727dmsh5124d1928e620c4p15e753jsn176e1783c5eb",
+          "x-rapidapi-host": "tvshow.p.rapidapi.com",
+        },
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getMovies();
+  }, []);
+
   return (
     <div
       className="flex justify-center items-center"
