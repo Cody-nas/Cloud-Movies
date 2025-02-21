dbaadcdd65609c9a9d918d67bcf88ce94f183adf
Cody-nas <nasiruadamu214@gmail.com>
2025-02-21 11:49:58 +0100

    Enhance Navigation component with arrow icons and improved styling

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import { IoMdArrowDropleftCircle } from "react-icons/io";
+import { IoMdArrowDroprightCircle } from "react-icons/io";
 
 const Navigation = () => {
   return (
-    <div>
-      <div>
-        <span>left arrow</span>
+    <div className="relative z-10">
+      <div className="fixed bottom-5 left-5 flex items-center gap-x-2 text-2xl bg-yellow-500/50 rounded-full px-2">
+        <span>
+          <IoMdArrowDropleftCircle />
+        </span>
         <p>1</p>
-        <span>right arrow</span>
+        <span>
+          <IoMdArrowDroprightCircle />
+        </span>
       </div>
       <select>
         <option value="TopRated">Top Rated</option>
