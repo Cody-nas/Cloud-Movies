c384d94a49f591de3e191adc319eda5e93642a65
Cody-nas <nasiruadamu214@gmail.com>
2025-02-22 07:47:51 +0100

    Enhance Navigation component with improved styling and cursor pointer for navigation arrows

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -5,15 +5,15 @@ const Navigation = () => {
   return (
     <div className="relative z-10">
       <div className="fixed bottom-5 left-5 flex items-center gap-x-2 text-2xl bg-yellow-500/50 rounded-full px-2">
-        <span>
+        <span className="cursor-pointer">
           <IoMdArrowDropleftCircle />
         </span>
-        <p>1</p>
-        <span>
+        <p className="text-xl select-none ">1</p>
+        <span className="cursor-pointer">
           <IoMdArrowDroprightCircle />
         </span>
       </div>
-      <select>
+      <select className="fixed top-5 left-5 bg-gray-200/90 text-sm tracking-wide text-gray-700 uppercase rounded-md outline-none p-1 cursor-pointer hover:bg-gray-200">
         <option value="TopRated">Top Rated</option>
         <option value="Popular">Popular</option>
         <option value="Upcoming">Upcoming</option>
